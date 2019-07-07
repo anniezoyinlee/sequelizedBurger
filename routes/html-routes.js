c1ca6a1fa3c8266c89b4ba6aabdb631ad7782b22
Annie Zoyin Lee <annie80113@hotmail.com>
2019-07-06 23:24:49 -0400

    updated files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,15 +1,7 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
 // Requiring our models
 var db = require("../models");
 
 // Routes
-// =============================================================
 module.exports = function(app) {
 
     app.get("/", function(req, res) {

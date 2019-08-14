84567b890b2a9371231eae4aee843e5263623654
Annie Zoyin Lee <annie80113@hotmail.com>
2019-08-13 20:22:35 -0400

    redo with skeleton

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-// Dependencies
 var express = require("express");
 
-// Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// Requiring our models for syncing
-var db = require("./models");
-
-// Sets up the Express app to handle data parsing
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
-app.use(express.static("public"));
-
-// Routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes")(app);
-
-
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller");
 
-// Syncing our sequelize models and then starting our Express app
+app.use(routes);
+
+// listen on port 3000
+var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT " + PORT);
 });
-db.sequelize.sync({ force: true }).then(function() {});
--- server.sql
+++
@@ -1,4 +0,0 @@
--- Drops the burgerlist if it exists currently --
-DROP DATABASE IF EXISTS burgerlist;
--- Creates the "burgerlist" database --
-CREATE DATABASE burgerlist;
